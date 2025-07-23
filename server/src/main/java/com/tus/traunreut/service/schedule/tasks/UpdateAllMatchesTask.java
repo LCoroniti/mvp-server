dac43a95dd81139abf2788b694ba01d34e07b944
SenpaiJannik <jannik.korbach@t-online.de>
2025-07-23 20:49:11 +0200

    Remove injected fields

--- server/src/main/java/com/tus/traunreut/service/schedule/tasks/UpdateAllMatchesTask.java
+++ server/src/main/java/com/tus/traunreut/service/schedule/tasks/UpdateAllMatchesTask.java
@@ -8,89 +8,20 @@ import com.tus.traunreut.repository.MatchRepository;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
 import org.jsoup.nodes.Element;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 @Entity
 @DiscriminatorValue("1")
 public class UpdateAllMatchesTask extends ScheduledTask {
-    //TODO: move Logger to e.g ScheduledTask so that each task uses the same logger
-    private static final Logger databaseLogger = LoggerFactory.getLogger("DATABASE");
-    private final MatchRepository matchRepository;
-    private final LeagueRepository leagueRepository;
-    private final MatchParsingService matchParsingService;
-    private final ScraperFactory scraperFactory;
-
-    @Autowired
-    public UpdateAllMatchesTask(MatchRepository matchRepository,
-                                LeagueRepository leagueRepository,
-                                MatchParsingService matchParsingService,
-                                ScraperFactory scraperFactory
-    ) {
-        this.matchRepository = matchRepository;
-        this.matchParsingService = matchParsingService;
-        this.leagueRepository = leagueRepository;
-        this.scraperFactory = scraperFactory;
-    }
-
-    // This task should run at the start up of the server and once a week
-    @Override
-    public void execute() {
-        // to update all matches in the database
-        // Get all leagues, fetch data of all matches (+ MeetingID), update
-
-        try {
-            //for each league:
-            List<String> leagues = leagueRepository.findDistinctLeagueNames();
-            if (leagues == null || leagues.isEmpty()) {
-                databaseLogger.warn("No leagues found in the database. Cannot update matches.");
-                return;
-            }
-            for (String leagueName : leagues) {
-                // Step 1: Fetch raw table rows using MatchScraper
-                Optional<League> optLeague = leagueRepository.findByName(leagueName);
-                if (optLeague.isEmpty())
-                {
-                    continue;
-                }
-                League league = optLeague.get();
-                IScraper<List<Element>> matchScraper = scraperFactory.createMatchScraper(league);
-                List<Element> rows = matchScraper.fetchData();
-                //rows is the table -> needs to be parse see MatchScraper commented code
-                // Step 2: Parse the rows into Match entities
-                List<Match> matches = matchParsingService.parseTableData(rows, league);
-                if (matches != null && !matches.isEmpty()) {
-                    //for each match in matches: get matchID and update data
-                    matches.forEach(match -> {
-                        // Step 3: Get match ID using MatchIDScraper
-                        MatchIDScraper matchIdScraper = new MatchIDScraper(leagueName, match);
-                        String matchId = matchIdScraper.fetchData();
-                        if (matchId != null && !matchId.isEmpty()) {
-                            match.setNuligaMatchId(matchId);
-                            // Step 4: Save or update the match in the database
-                            matchRepository.saveOrUpdate(match);
-                        } else {
-                            databaseLogger.warn("No match ID found for match: {} vs {} in league: {}",
-                                    match.getHomeTeam().getName(),
-                                    match.getGuestTeam().getName(),
-                                    leagueName);
-                        }
-                    });
-                }
-                else {
-                    databaseLogger.warn("No matches found for league: {}", leagueName);
-                }
-
-            }
-            databaseLogger.info("UpdateAllMatchesTask executed successfully.");
-        } catch (Exception e) {
-            databaseLogger.error("Error occurred while executing UpdateAllMatchesTask: ", e);
-        }
-
+    public UpdateAllMatchesTask(LocalDateTime executionTime) {
+        super(executionTime);
     }
 }
--- server/src/main/java/com/tus/traunreut/service/schedule/tasks/UpdateSingleMatchTask.java
+++ server/src/main/java/com/tus/traunreut/service/schedule/tasks/UpdateSingleMatchTask.java
@@ -4,18 +4,12 @@ import com.tus.traunreut.ScheduledTask;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 
+import java.time.LocalDateTime;
+
 @Entity
 @DiscriminatorValue("3")
 public class UpdateSingleMatchTask extends ScheduledTask {
-    @Override
-    public void execute() {
-        // get match by ID, fetch data of the match, update
-        // This task should run after a match to get the final result and during the match to update score??
-
-        // Use MatchScraper to fetch the match data
-        // -> returns List of Matches
-        // -> get match by ID from List
-        // -> set/update data of received match to database
-
+    public UpdateSingleMatchTask(LocalDateTime executionTime) {
+        super(executionTime);
     }
 }
