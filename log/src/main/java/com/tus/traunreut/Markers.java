4fcb958d7304698eb60e0252cc74ee4280495128
SenpaiJannik <jannik.korbach@t-online.de>
2025-07-24 20:45:33 +0200

    Loggers initialized in central place. Use these loggers instead of instantiating new ones

--- log/src/main/java/com/tus/traunreut/Markers.java
+++ log/src/main/java/com/tus/traunreut/Markers.java
@@ -1,10 +1,17 @@
 package com.tus.traunreut;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.slf4j.Marker;
 import org.slf4j.MarkerFactory;
 
 public class Markers {
     public final static Marker NETWORK = MarkerFactory.getMarker("NETWORK");
     public final static Marker DATABASE = MarkerFactory.getMarker("DATABASE");
+    public final static Marker INTERNAL = MarkerFactory.getMarker("INTERNAL");
+
+    public final static Logger DB_LOG = LoggerFactory.getLogger("DATABASE");
+    public final static Logger NETWORK_LOG = LoggerFactory.getLogger("NETWORK");
+    public final static Logger INTERNAL_LOG = LoggerFactory.getLogger("INTERNAL");
 }
