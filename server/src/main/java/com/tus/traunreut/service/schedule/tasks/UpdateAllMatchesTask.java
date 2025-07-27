d797aaff63c27eeb75edce81d1be00291295faf5
SenpaiJannik <jannik.korbach@t-online.de>
2025-07-27 20:45:48 +0200

    Add no args constructor

--- server/src/main/java/com/tus/traunreut/service/schedule/tasks/UpdateAllMatchesTask.java
+++ server/src/main/java/com/tus/traunreut/service/schedule/tasks/UpdateAllMatchesTask.java
@@ -1,25 +1,16 @@
 package com.tus.traunreut.service.schedule.tasks;
 
-import com.tus.traunreut.*;
-import com.tus.traunreut.repository.LeagueRepository;
-import com.tus.traunreut.scraper.MatchIDScraper;
-import com.tus.traunreut.service.MatchParsingService;
-import com.tus.traunreut.repository.MatchRepository;
-
+import com.tus.traunreut.ScheduledTask;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
-import lombok.AllArgsConstructor;
-import org.jsoup.nodes.Element;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
 
 @Entity
 @DiscriminatorValue("1")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UpdateAllMatchesTask extends ScheduledTask {
     public UpdateAllMatchesTask(LocalDateTime executionTime) {
         super(executionTime);
