0e958aa3272f6d3bff52514c16daaeacebe7d426
SenpaiJannik <jannik.korbach@t-online.de>
2025-07-29 21:14:26 +0200

    Add new task for fetching nuliga meeting id

---
+++ server/src/main/java/com/tus/traunreut/service/schedule/executors/impl/GetNuLigaTaskExecutor.java
@@ -0,0 +1,12 @@
+package com.tus.traunreut.service.schedule.executors.impl;
+
+import com.tus.traunreut.service.schedule.executors.ScheduledTaskExecutor;
+import com.tus.traunreut.service.schedule.tasks.GetNuLigaIdTask;
+
+public class GetNuLigaTaskExecutor implements ScheduledTaskExecutor<GetNuLigaIdTask> {
+
+    @Override
+    public void execute(GetNuLigaIdTask task) {
+
+    }
+}
