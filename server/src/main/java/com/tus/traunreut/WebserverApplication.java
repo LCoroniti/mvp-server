8b55d04f4ff3a05e1d6baa1f514ba6c51e617297
SenpaiJannik <jannik.korbach@t-online.de>
2025-09-18 20:12:36 +0200

    Fix main class

--- server/src/main/java/com/tus/traunreut/WebserverApplication.java
+++ server/src/main/java/com/tus/traunreut/WebserverApplication.java
@@ -1,7 +1,5 @@
 package com.tus.traunreut;
 
-import org.apache.logging.log4j.Marker;
-import org.apache.logging.log4j.MarkerManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
